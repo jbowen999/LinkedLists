394fe8c14034d2da061890a1f942617cefdc1f78
jbowen999 <julian.e.bowen@outlook.com>
2024-07-26 11:40:18 -0700

    Add project files.

---
+++ LinkedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedLists
+{
+    public class LinkedList
+    {
+        public Node? First { get; set; }
+
+        public void InsertFirst(int data)
+        {
+            // Create the node
+            Node newNode = new Node();
+            // Put the data in the node
+            newNode.Data = data;
+            // Put the old node in next
+            newNode.Next = First;
+            // Make the first new node
+            First = newNode;
+        }
+        public Node DeleteFirst()
+        {
+            // Assign the temporaty variable
+            Node temp = First;
+            //Assign the next  dead
+            return temp;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace LinkedLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Node nodeA = new Node();
+            nodeA.Data = 335;
+            Node nodeB = new Node();
+            nodeB.Data = 989;
+            Node nodeC = new Node();
+            nodeC.Data = 322;
+            Node nodeD = new Node();
+            nodeD.Data = 555;
+
+            nodeA.Next = nodeB;
+            nodeB.Next = nodeC;
+            nodeC.Next = nodeD;
+        }
+    }
+}
